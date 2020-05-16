44fa474f90fbebccc8d95487db84bba0108fa5bd
ANUP VICTOR DSOUZA <65356304+victoravd@users.noreply.github.com>
2020-05-16 09:33:53 -0500

    Update Hello_world.c

--- Hello_world.c
+++ Hello_world.c
@@ -2,13 +2,35 @@
 int main()
 {
 	printf("Hello Team 2\n");
-	char team_member = "anup";
+	char team_member[] = "anup";
 	switch (team_member)
 	{
 		case "anup:
 			anup_excerice();
+			
+		case "oliver":
+			exit;
+			
+		case "vipin":
+			exit;
+			
+		case "maurice":
+			exit;
+		
+		case "harris":
+			exit;
+			
+		default:
+			do_nothing();
 	}
 	
 	
 	return 0;
 }
+
+void anup_exerice()
+{
+	return 0;
+}
+
+
