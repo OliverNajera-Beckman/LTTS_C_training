b8dca40556704d0790f92534ba87f6de69935772
ANUP VICTOR DSOUZA <65356304+victoravd@users.noreply.github.com>
2020-05-16 14:41:37 -0500

    Update Hello_world.c

--- Hello_world.c
+++ Hello_world.c
@@ -3,6 +3,30 @@
 int main()
 {
 	printf("Hello Team 2\n");
+	char team2 = 'A' 
+	
+	switch(team2)
+	{
+		case 'A':
+			printf("Anup to write function");
+			break;
+		case 'O':
+			printf("Oliver to write function");
+			break;
+		case 'M':
+			printf("Maurice to write function");
+			break;
+		case 'V':
+			printf("Vipin to write function");
+			break;
+		case 'H':
+			printf("Harris to write function");
+			break;
+		default:
+			printf("Enter correct team member");
+			
+	}
+	
 	return 0;
 }
 
