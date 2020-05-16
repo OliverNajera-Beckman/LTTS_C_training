73fcc71bdf0dbb342ba894be11a40a8b65568d69
ANUP VICTOR DSOUZA <65356304+victoravd@users.noreply.github.com>
2020-05-16 09:52:10 -0500

    Update Hello_world.c

--- Hello_world.c
+++ Hello_world.c
@@ -3,34 +3,6 @@
 int main()
 {
 	printf("Hello Team 2\n");
- 	team2("anup");
 	return 0;
 }
 
-void team2 (char name[10]) {
-    switch (name[10]) 
-    {
-        case name["anup"]:
-            printf("write your function here\n");
-	    break;
-		    
-        case name["oliver"]:
-            printf("write your function here\n");
-	    break;
-		    
-        case name["vipin"] :
-            printf("write your function here\n");
-	    break;
-		    
-        case name["harris"] :
-            printf("write your function here\n");
-	    break;
-		    
-        case name["maurice"] :
-            printf("write your function here\n");		    
-	    break;
-		    
-        default :
-            printf("Enter a valid choice: \n");
-    }
-}
