3e07df9b328092235636cb48e596b119b8a33684
Oliver Najera <riverside_u@yahoo.com>
2020-05-17 18:33:45 -0500

    folder for my file

---
+++ oliver.c
@@ -0,0 +1,17 @@
+/*
+ * oliver.c
+ *
+ *  Created on: May 16, 2020
+ *      Author: najera
+ */
+#include <string.h>
+
+string concat2strings(){
+	string word1,word2,word3;
+
+	word1="stay";
+	word2="healthy";
+
+	word3= word1+" "+word2;
+	return word3;
+}
