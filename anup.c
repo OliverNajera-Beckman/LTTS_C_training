ee44434d244feed9b8ea923def06a4d8464423ea
ANUP VICTOR DSOUZA <65356304+victoravd@users.noreply.github.com>
2020-05-18 14:30:11 -0500

    Update anup.c

--- anup.c
+++ anup.c
@@ -1,7 +1,7 @@
 #include "team2.h"
 #include <stdio.h>
 
-void swap2numbers()
+/*void swap2numbers()
 {
   printf("TEST");
   int a = 10, b = 20;
@@ -10,4 +10,62 @@ void swap2numbers()
   temp = a;
   a = b;
   b = temp;
+}*/
+
+void Set_Overfrequency_Fault()
+{
+	int Frequency = 0;
+
+	// Read Frequency
+	Frequency = Read_Freq();
+
+	// Wait 5 seconds if Frequency > 420 Hz
+	if (Frequency > 420)
+	{
+		OF_FAULT = TRUE;
+	}
+
+	if (OF_FAULT == TRUE)
+	{
+		Timer(OVER_FREQUENCY_TIMER);
+		log_nvm(2025);
+	}
+	else
+	{
+		log_nvm(0);
+	}
+	printf("OF_FAULT = %d\n", OF_FAULT);
+}
+
+int Read_Freq()
+{
+    // Converting time into milli_seconds
+    int freq;
+
+   // printf("Enter the frequency\n");
+    scanf("%d", &freq);
+
+    return freq;
+
+}
+
+void log_nvm(int fault_code)
+{
+
+	FAULT_CODE = fault_code;
+	//printf("FAULT_CODE = %d\n", FAULT_CODE);
+
+}
+
+void Timer(int number_of_seconds)
+{
+    // Converting time into milli_seconds
+    int milli_seconds = 1000 * number_of_seconds;
+
+    // Storing start time
+    clock_t start_time = clock();
+
+    // looping till required time is not achieved
+    while (clock() < start_time + milli_seconds)
+        ;
 }
