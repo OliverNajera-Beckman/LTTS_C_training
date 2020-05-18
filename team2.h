5b33ab0ca201f5da66c722b7c36a601f14a1861a
ANUP VICTOR DSOUZA <65356304+victoravd@users.noreply.github.com>
2020-05-18 14:31:39 -0500

    Update team2.h

--- team2.h
+++ team2.h
@@ -6,7 +6,20 @@
 #include <string.h>
 
 /* Anup***********************************/
-void swap2numbers();
+//void swap2numbers();
+void Timer (int number_of_seconds);
+int Read_Freq ();
+void log_nvm(int fault_code);
+void Set_Overfrequency_Fault();
+
+#define OVER_FREQUENCY_TIMER 5
+#define TRUE 1
+#define FALSE 0
+
+int OF_FAULT = 0;
+int FAULT_CODE = 0;
+/* Anup***********************************/
+
 
 //string concat2strings();
 int countsNumberOfDigits(long long n);
