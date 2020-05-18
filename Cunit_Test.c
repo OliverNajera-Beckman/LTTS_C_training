bd64defc57ae018d8b01a1156e524cb1a123de9a
Vipin <65356358+Vipin-Chourasia@users.noreply.github.com>
2020-05-17 19:32:14 -0500

    Created for Cunit Test

---
+++ Cunit_Test.c
@@ -0,0 +1,42 @@
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
+#include "team2.h"
+
+int init_suite(void) { return 0; }
+int clean_suite(void) { return 0; }
+
+int maxi(int i1, int i2)
+{
+    return (i1 > i2) ? i1 : i2;
+}
+
+void test_maxi(void)
+{
+    CU_ASSERT(maxi(0,2) == 2);
+}
+
+int main() {
+    // Initialize CUnit test registry
+    if (CUE_SUCCESS != CU_initialize_registry())
+    return CU_get_error();
+
+    // Add suite_maxi to registry
+    CU_pSuite suite_maxi = CU_add_suite("maxi_test", init_suite, clean_suite);
+    if (NULL == suite_maxi) 
+    {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    // add test_maxi to suite_maxi
+    if ((NULL == CU_add_test(suite_maxi, "maxi_fun", test_maxi)))
+    {
+      CU_cleanup_registry();
+      return CU_get_error();
+    }
+    
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests(); // OUTPUT to the screen
+    CU_cleanup_registry();
+    return CU_get_error();
+}
