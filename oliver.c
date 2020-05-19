e15323aa28e65440f984ef1c4f5ab3b880e9fbb1
najer <najer@32015384>
2020-05-18 19:32:49 -0500

    My revision of my component function

--- oliver.c
+++ oliver.c
@@ -7,11 +7,11 @@
 #include <string.h>
 
 string concat2strings(){
-	string word1,word2,word3;
+	char word1[50],word2[50],word3[100];
 
 	word1="stay";
 	word2="healthy";
 
-	word3= word1+" "+word2;
+	word3= word1+word2;
 	return word3;
 }
